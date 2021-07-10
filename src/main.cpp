8f8b1f69e33919e9dfc5ec69819dc5010776b0a7
coon <coon@mailbox.org>
2021-07-10 02:45:29 +0200

    implement basic wxApplication

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,73 @@
 #include <iostream>
+#include <wx/wx.h>
 
-int main( ) {
-  printf("Hello World!\n");
+//--------------------------------------------------------------------------------------------------------------
+// WxApp
+//--------------------------------------------------------------------------------------------------------------
+
+class WxApp : public wxApp {
+public:
+  virtual bool OnInit();
+};
+
+//--------------------------------------------------------------------------------------------------------------
+// MainFrame
+//--------------------------------------------------------------------------------------------------------------
+
+class MainFrame : public wxFrame {
+public:
+  MainFrame(const wxString& title, const wxPoint& pos, const wxSize& size);
+
+private:
+  void OnExit(wxCommandEvent& event);
+  void OnAbout(wxCommandEvent& event);
+
+  wxDECLARE_EVENT_TABLE();
+};
+
+wxBEGIN_EVENT_TABLE(MainFrame, wxFrame)
+EVT_MENU(wxID_EXIT, MainFrame::OnExit)
+EVT_MENU(wxID_ABOUT, MainFrame::OnAbout)
+wxEND_EVENT_TABLE()
+wxIMPLEMENT_APP_CONSOLE(WxApp);
+
+bool WxApp::OnInit() {
+  MainFrame *frame = new MainFrame("Floppy Music DAW", wxPoint(50, 50), wxSize(450, 340));
+  frame->Show(true);
+
+  printf("Floppy Music DAW started.\n");
+  return true;
+}
+
+//--------------------------------------------------------------------------------------------------------------
+// MainFrame
+//--------------------------------------------------------------------------------------------------------------
+
+MainFrame::MainFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
+  : wxFrame(NULL, wxID_ANY, title, pos, size) {
+  wxMenu *menuFile = new wxMenu;
+  menuFile->Append(wxID_EXIT);
+
+  wxMenu *menuHelp = new wxMenu;
+  menuHelp->Append(wxID_ABOUT);
+
+  wxMenuBar *menuBar = new wxMenuBar;
+  menuBar->Append(menuFile, "&File");
+  menuBar->Append(menuHelp, "&Help");
+
+  SetMenuBar(menuBar);
+  CreateStatusBar();
+  SetStatusText("Ready.");
+}
+
+void MainFrame::OnExit(wxCommandEvent& event) {
+  Close(true);
+}
+
+void MainFrame::OnAbout(wxCommandEvent& event) {
+  wxMessageBox("Made by coon\n\n"
+               "E-Mail: coon@mailbox.org\n"
+               "IRC: coon@hackint.org\n\n"
+               "https://www.reddit.com/user/coon42\nhttps://github.com/coon42\n",
+               "Floppy Music DAW", wxOK | wxICON_INFORMATION);
 }
