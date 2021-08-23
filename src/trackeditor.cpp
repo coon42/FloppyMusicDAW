4a9f988426600f2af50630e48b908caf5bb61bf2
coon <coon@mailbox.org>
2021-08-23 23:18:50 +0200

    TrackEditorWindow: create row for each track which is present in Song::tracks_

--- src/trackeditor.cpp
+++ src/trackeditor.cpp
@@ -8,7 +8,7 @@ TrackEditorWindow::TrackEditorWindow(wxWindow* pParent, Song* pSong)
     : wxWindow(pParent, wxID_ANY, wxDefaultPosition, wxDefaultSize), pSong_(pSong) {
 
   pTrackWindow_ = new wxGrid(this, wxID_ANY, wxDefaultPosition, wxDefaultSize);
-  pTrackWindow_->CreateGrid(1, 3);
+  pTrackWindow_->CreateGrid(pSong_->numberOfTracks(), 3);
   pTrackWindow_->HideRowLabels();
   pTrackWindow_->SetSelectionMode(wxGrid::wxGridSelectionModes::wxGridSelectNone);
 
@@ -16,17 +16,19 @@ TrackEditorWindow::TrackEditorWindow(wxWindow* pParent, Song* pSong)
   pTrackWindow_->SetColLabelValue(1, "Track Name");
   pTrackWindow_->SetColLabelValue(2, "Channel");
 
-  pTrackWindow_->SetCellValue(0, 0, "X");
-  pTrackWindow_->SetCellValue(0, 1, pSong_->track(0)->name());
-  pTrackWindow_->SetCellValue(0, 2, wxString::Format("%d", pSong_->track(0)->midiChannel() + 1));
+  for (int trackNo = 0; trackNo < pSong_->numberOfTracks(); ++trackNo) {
+    pTrackWindow_->SetCellValue(trackNo, 0, trackNo == 0 ? "X" : "");
+    pTrackWindow_->SetCellValue(trackNo, 1, pSong_->track(trackNo)->name());
+    pTrackWindow_->SetCellValue(trackNo, 2, wxString::Format("%d", pSong_->track(trackNo)->midiChannel() + 1));
 
-  for (int row = 0; row < pTrackWindow_->GetNumberRows(); ++row) {
-    for (int col = 0; col < pTrackWindow_->GetNumberCols(); ++col) {
-      pTrackWindow_->SetReadOnly(row, col);
+    for (int row = 0; row < pTrackWindow_->GetNumberRows(); ++row) {
+      for (int col = 0; col < pTrackWindow_->GetNumberCols(); ++col) {
+        pTrackWindow_->SetReadOnly(row, col);
 
-      pTrackWindow_->SetCellAlignment(row, col, wxALIGN_CENTER, 0);
-      pTrackWindow_->SetCellAlignment(row, col, wxALIGN_CENTER, 0);
-      pTrackWindow_->SetCellAlignment(row, col, wxALIGN_CENTER, 0);
+        pTrackWindow_->SetCellAlignment(row, col, wxALIGN_CENTER, 0);
+        pTrackWindow_->SetCellAlignment(row, col, wxALIGN_CENTER, 0);
+        pTrackWindow_->SetCellAlignment(row, col, wxALIGN_CENTER, 0);
+      }
     }
   }
 
