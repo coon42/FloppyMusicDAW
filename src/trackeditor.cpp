0812bfe11fc65ce2e846746f7b7d2f767ca07f80
coon <coon@mailbox.org>
2022-03-13 21:53:56 +0100

    trackeditor: add track length column

--- src/trackeditor.cpp
+++ src/trackeditor.cpp
@@ -8,13 +8,14 @@ TrackEditorWindow::TrackEditorWindow(wxWindow* pParent, Song* pSong)
     : wxWindow(pParent, wxID_ANY, wxDefaultPosition, wxDefaultSize), pSong_(pSong) {
 
   pTrackListGrid_ = new wxGrid(this, wxID_ANY, wxDefaultPosition, wxDefaultSize);
-  pTrackListGrid_->CreateGrid(pSong_->numberOfTracks(), 3);
+  pTrackListGrid_->CreateGrid(pSong_->numberOfTracks(), 4);
   pTrackListGrid_->HideRowLabels();
   pTrackListGrid_->SetSelectionMode(wxGrid::wxGridSelectionModes::wxGridSelectNone);
 
   pTrackListGrid_->SetColLabelValue(0, "Selected");
   pTrackListGrid_->SetColLabelValue(1, "Track Name");
   pTrackListGrid_->SetColLabelValue(2, "Channel");
+  pTrackListGrid_->SetColLabelValue(3, "Duration");
 
   pTrackListGrid_->SetColLabelSize(pTrackListGrid_->GetCharHeight() + 4);
 
@@ -35,7 +36,14 @@ void TrackEditorWindow::updateTrackList() {
     pTrackListGrid_->SetCellValue(trackNo, 0, trackNo == 0 ? "X" : "");
     pTrackListGrid_->SetCellValue(trackNo, 1, pSong_->track(trackNo)->name());
     pTrackListGrid_->SetCellValue(trackNo, 2, wxString::Format("%d", pSong_->track(trackNo)->midiChannel() + 1));
-
+        
+    const uint64_t us = pSong_->track(trackNo)->durationUs();    
+    const uint32_t m  = (us / 60) / 1000000;
+    const uint32_t s  = us / 1000000 - m * 60;
+    const uint32_t roundedMs = (us - m * 60 * 1000000 - s * 1000000 + 500) / 1000;  
+        
+    pTrackListGrid_->SetCellValue(trackNo, 3, wxString::Format("%02d:%02d:%03d", m, s, roundedMs));
+    
     for (int row = 0; row < pTrackListGrid_->GetNumberRows(); ++row) {
       for (int col = 0; col < pTrackListGrid_->GetNumberCols(); ++col) {
         pTrackListGrid_->SetReadOnly(row, col);
